7ee0e0ae9d03688dec5cab5e8b59a6371c8b513d
Epicmanpreet02 <144267040+Epicmanpreet01@users.noreply.github.com>
2025-06-20 23:32:22 +0530

    created ui for login and signup pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
+import LogInPage from './pages/auth/logIn/logInPage.jsx'
+import SignUpPage from './pages/auth/signUp/SignUpPage.jsx'
+import HomePage from './pages/home/HomePage.jsx'
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-xl font-medium bg-amber-500">Hello</h1>
-    </>
+    <div className='flex mx-auto max-w-6xl'>
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+        <Route path='/SignUp' element={<SignUpPage />}/>
+        <Route path='/LogIn' element={<LogInPage />}/>
+      </Routes>
+    </div>
   )
 }
 
---
+++ frontend/src/pages/auth/signUp/SignUpPage.jsx
@@ -0,0 +1,102 @@
+import { useState } from 'react'
+import XSvg from "../../../components/svgs/X";
+import { Link } from 'react-router-dom'; 
+import { MdOutlineMail } from "react-icons/md";
+import { FaUser } from "react-icons/fa";
+import { MdPassword } from "react-icons/md";
+import { MdDriveFileRenameOutline } from "react-icons/md";
+
+const SignUpPage = () => {
+
+  const [formData,setFormData] = useState({
+    email: '',
+    name: '',
+    userName: '',
+    password: ''
+  });
+
+  function handleChange(e) {
+    const { name,value } = e.target;
+
+    setFormData(prevFormData => ({
+      ...prevFormData,
+        [name]:value
+    }))
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formData);
+  }
+
+  const isError = false
+
+  return (
+    <div className='flex max-w-screen-xl mx-auto px-10 h-screen w-full'>
+      <div className="flex-1 hidden lg:flex items-center justify-center">
+        <XSvg className="lg:w-2/3 fill-white" />
+      </div>
+
+      <div className='flex-1 flex flex-col justify-center items-center w-full'>
+        <form className='lg:w-2/3 mx-auto md:mx-20 flex gap-4 flex-col w-full' onSubmit={handleSubmit}>
+          <XSvg className="w-24 lg:hidden fill-white" />
+          <h1 className='text-4xl font-extrabold text-white '>Join today.</h1>
+          <label className='input input-bordered rounded flex items-center gap-2 w-full'>
+            <MdOutlineMail />
+            <input 
+              type="email" 
+              className='grow' 
+              placeholder='Email'
+              name='email'
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </label>
+          <label className='input input-bordered rounded flex items-center gap-2 w-full'>
+            <FaUser />
+            <input 
+              type="text" 
+              className='grow' 
+              placeholder='Username'
+              name='userName'
+              value={formData.userName}
+              onChange={handleChange}
+            />
+          </label>
+          <label className='input input-bordered rounded flex items-center gap-2 w-full'>
+            <MdDriveFileRenameOutline />
+            <input 
+              type="text" 
+              className='grow' 
+              placeholder='Full Name'
+              name='name'
+              value={formData.name}
+              onChange={handleChange}
+            />
+          </label>
+          <label className='input input-bordered rounded flex items-center gap-2 w-full'>
+            <MdPassword />
+            <input 
+              type="password" 
+              className='grow' 
+              placeholder='Password'
+              name='password'
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </label>     
+          <button className='btn rounded-full btn-primary text-white'>Sign up</button>
+          {isError && <p className='text-error'>Something went wrong</p>}
+        </form>
+        <div className='flex flex-col lg:w-2/3 gap-2 mt-4'>
+          <p className='text-white text-md'>Already have an account?</p>
+          <Link to="/LogIn">
+            <button className='btn btn-outline btn-primary rounded-full text-white w-full mt-3'>Sign In</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SignUpPage
