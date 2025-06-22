2536912e00c45894ca8510f6ee9eb0c2920edde0
Epicmanpreet02 <144267040+Epicmanpreet01@users.noreply.github.com>
2025-06-22 16:18:35 +0530

    cleaned up the skeleton usage a little

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -4,19 +4,19 @@ import usePosts from "../../hooks/queries/usePosts";
 
 const Posts = ({ feedtype }) => {
 
-	const { data: posts, isLoading} = usePosts(feedtype)
+	const { data: posts, isLoading,isFetching} = usePosts(feedtype)
 
 	return (
 		<>
-			{isLoading && (
+			{isFetching && isLoading && (
 				<div className='flex flex-col justify-center'>
 					<PostSkeleton />
 					<PostSkeleton />
 					<PostSkeleton />
 				</div>
 			)}
-			{!isLoading && posts?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
-			{!isLoading && posts && (
+			{!isFetching && !isLoading && posts?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
+			{!isFetching && !isLoading && posts && (
 				<div>
 					{posts.map((post) => (
 						<Post key={post._id} feedtype={feedtype} post={post} />
